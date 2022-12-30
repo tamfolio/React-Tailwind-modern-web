3c2368ba8444226a23577d0932d26f8229e68fe6
Tamilore <tamiloreolaniyi@gmail.com>
2022-12-30 07:22:29 +0100

    hero section again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../styles'
 import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
 function Hero() {
   return (
@@ -16,6 +17,19 @@ function Hero() {
               <span className='text-white'>20%</span> DISCOUNT FOR {" "}
               <span className='text-white'>1 Month</span> Account
               </p>
+              </div>
+              <div className='flex flex-row justify-between items-center
+              w-full'>
+                     <h1 className='flex-1 font-poppins 
+                     font-semibold ss:text-[72px] text-[52px]
+                     text-white ss:leading-[100px] leading-[75px]'>The Next 
+                      <br className='sm:block hidden'/>{" "}
+                      <span className='text-gradient'>Generation</span> {" "}
+                       Payment Method
+                      </h1>          
+                      <div className='ss:flex hidde md:mr-4 mr-0'>
+                        <GetStarted/>  
+                      </div>   
               </div>
         </div>
     </section>
